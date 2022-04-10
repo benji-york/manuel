18982a4c7391a108224990332e65a6adb694ed33
ksuess <k.suess@rohberg.ch>
2022-04-10 16:59:09 +0200

    Allow MyST markdown documentation.

--- src/manuel/codeblock.py
+++ src/manuel/codeblock.py
@@ -1,11 +1,20 @@
 import re
-import manuel
 import textwrap
 
-CODEBLOCK_START = re.compile(
-    r'(^\.\.\s*(invisible-)?code(-block)?::?\s*python\b(?:\s*\:[\w-]+\:.*\n)*)',
-    re.MULTILINE)
-CODEBLOCK_END = re.compile(r'(\n\Z|\n(?=\S))')
+import manuel
+
+CODEBLOCK_START_RST = re.compile(
+    r"(^\.\.\s*(invisible-)?code(-block)?::?\s*python\b(?:\s*\:[\w-]+\:.*\n)*)",
+    re.MULTILINE,
+)
+CODEBLOCK_END_RST = re.compile(r"(\n\Z|\n(?=\S))")
+
+CODEBLOCK_START_MYST = re.compile(
+    r"(^(```python)|(% invisible-code-block:\s+python)$)",
+    re.MULTILINE,
+)
+CODEBLOCK_END_MYST = re.compile(r"(\n(?=(```)?\n))")
+#  foo_end = re.compile('(\\n(?=(```)?\\n\\n))')
 
 
 class CodeBlock(object):
@@ -15,11 +24,21 @@ class CodeBlock(object):
 
 
 def find_code_blocks(document):
+    CODEBLOCK_START = (
+        document.location.endswith(".md")
+        and CODEBLOCK_START_MYST
+        or CODEBLOCK_START_RST
+    )
+    CODEBLOCK_END = (
+        document.location.endswith(".md") and CODEBLOCK_END_MYST or CODEBLOCK_END_RST
+    )
     for region in document.find_regions(CODEBLOCK_START, CODEBLOCK_END):
         start_end = CODEBLOCK_START.search(region.source).end()
         source = textwrap.dedent(region.source[start_end:])
-        source_location = '%s:%d' % (document.location, region.lineno)
-        code = compile(source, source_location, 'exec', 0, True)
+        # MyST comments
+        source = re.sub(r'\n%[ ]?', '\n', source)
+        source_location = "%s:%d" % (document.location, region.lineno)
+        code = compile(source, source_location, "exec", 0, True)
         document.claim_region(region)
         region.parsed = CodeBlock(code, source)
 
@@ -29,7 +48,7 @@ def execute_code_block(region, document, globs):
         return
 
     exec(region.parsed.code, globs)
-    del globs['__builtins__'] # exec adds __builtins__, we don't want it
+    del globs["__builtins__"]  # exec adds __builtins__, we don't want it
 
 
 class Manuel(manuel.Manuel):
---
+++ src/manuel/myst-example.md
@@ -0,0 +1,20 @@
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+
+```python
+assert 2 == 2
+```
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+
+```python
+assert 3 == 3
+```
+
+And now some invisible code (not for documenation).
+
+% invisible-code-block: python
+%
+% assert 4 == 4
+% assert 5 == 5
+
+Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
